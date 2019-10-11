a2c454b89999563a479a98ed3aed84248469ca90
Pol Monroig <polmonroig@yahoo.es>
2019-10-11 17:19:11 +0200

    Upload hierarchy

---
+++ main.py
@@ -0,0 +1,6 @@
+# load libraries
+from comet_ml import Experiment
+
+experiment = Experiment(api_key="a604AfX0S9Bmt6HdpMHxg9MCI",
+                        project_name="styletransfer", workspace="polmonroig")
+
